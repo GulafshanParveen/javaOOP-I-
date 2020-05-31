9a0d128435e933894d84ed24e7d261e6594b06cd
Gulafshan Parveen <64768393+GulafshanParveen@users.noreply.github.com>
2020-05-31 14:25:17 +0530

    Callmethod in oops

---
+++ CallMethod.java
@@ -0,0 +1,10 @@
+class CallMethod{
+	public static void main(String[] args){
+		//method called
+		myfunction();
+	}
+	public static void myfunction(){
+		//method executed
+		System.out.println("Method is call");
+	}
+}
