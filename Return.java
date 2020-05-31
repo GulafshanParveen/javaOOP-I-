6fe82fe68fba7ee0520096a94872114c6248ec24
Gulafshan Parveen <64768393+GulafshanParveen@users.noreply.github.com>
2020-05-31 14:26:22 +0530

    Return type

---
+++ Return.java
@@ -0,0 +1,10 @@
+class Return{
+	public static void main(String[] args){
+		int result;
+		result=data();
+		System.out.println("RESULT==" +result);
+	}
+	public static int data(){
+                return 10*10;
+        }
+}
